676fcd3538ee2c7fd4d3ec817158023bbd62a395
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-10 11:06:06 +0100

    add selectors

--- page.js
+++ page.js
@@ -1,12 +1,17 @@
 /* ----------------------------------- */
 //!             SELECTORS              */
 /* ----------------------------------- */
+const allInputs = document.querySelectorAll("input");
+const allMinusBtn = document.querySelectorAll("#minus");
+const allCountSpan = document.querySelectorAll("#count");
+const allPlusBtn = document.querySelectorAll("#plus");
+const allRemoveI = document.querySelectorAll(".remove");
+
+// for updating amounts
+const subTotalSpan = document.getElementById("sub-total");
+const taxSpan = document.getElementById("tax");
+const shippingInfoSpan = document.getElementById("shipping-info");
+const shippingPriceSpan = document.getElementById("shipping-price");
+const totalSpan = document.getElementById("total");
 
 
-/* ----------------------------------- */
-//!          EVENT LISTENERS           */
-/* ----------------------------------- */
-
-/* ----------------------------------- */
-//!             FUNCTIONS              */
-/* ----------------------------------- */
