be1b5b22cd02de027ce87b65c9f38e13b92e86b2
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-10 11:06:24 +0100

    add EventListeners

--- page.js
+++ page.js
@@ -14,4 +14,30 @@ const shippingInfoSpan = document.getElementById("shipping-info");
 const shippingPriceSpan = document.getElementById("shipping-price");
 const totalSpan = document.getElementById("total");
 
+/* ----------------------------------- */
+//!          EVENT LISTENERS           */
+/* ----------------------------------- */
 
+allInputs.forEach((input) => {
+  input.addEventListener("focusin", (e) => {
+    e.target.parentElement.classList.remove("opacity");
+  });
+
+  input.addEventListener("focusout", (e) => {
+    if (!e.target.value || e.target.checked == false) {
+      e.target.parentElement.classList.add("opacity"); // only add opacity, if its empty
+    }
+  });
+});
+
+allMinusBtn.forEach((minus) => {
+  minus.addEventListener("click", handleMinusBtnClick);
+});
+
+allPlusBtn.forEach((plus) => {
+  plus.addEventListener("click", handlePlusBtnClick);
+});
+
+allRemoveI.forEach((remove) => {
+  remove.addEventListener("click", handleRemoveIClick);
+});
