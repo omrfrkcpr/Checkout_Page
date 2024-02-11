dc9dab00e7b9b578c9ec4292d7d86939be2834a7
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-11 13:28:50 +0100

    page.js - fix bugs (update project functionality)

--- page.js
+++ page.js
@@ -1,156 +1,127 @@
-/* ----------------------------------- */
-//!             SELECTORS              */
-/* ----------------------------------- */
-const allInputs = document.querySelectorAll("input");
-const allMinusBtn = document.querySelectorAll("#minus");
-const allCountSpans = document.querySelectorAll(".count");
-const discountSpan = document.querySelector("#discount");
-const allPlusBtn = document.querySelectorAll("#plus");
-const allRemoveI = document.querySelectorAll(".remove");
-const allItemTotalSpan = document.querySelectorAll("#item-total");
+document.addEventListener("DOMContentLoaded", function () {
+  const currency = "€";
 
-// for updating amounts
-const subTotalSpan = document.getElementById("sub-total");
-const taxSpan = document.getElementById("tax");
-const shippingInfoSpan = document.getElementById("shipping-info");
-const shippingPriceSpan = document.getElementById("shipping-price");
-const totalSpan = document.getElementById("total");
-const currency = "€";
+  /* ---------------------------------- */
+  //!             Functions             */
+  /* ---------------------------------- */
 
-/* ----------------------------------- */
-//!          EVENT LISTENERS           */
-/* ----------------------------------- */
-
-allInputs.forEach((input) => {
-  input.addEventListener("focusin", (e) => {
-    e.target.parentElement.classList.remove("opacity");
-  });
-
-  input.addEventListener("focusout", (e) => {
-    if (!e.target.value || e.target.checked == false) {
-      e.target.parentElement.classList.add("opacity"); // only add opacity, if its empty
-    }
-  });
-});
-
-allMinusBtn.forEach((minus) => {
-  minus.addEventListener("click", handleMinusBtnClick);
-});
-
-allPlusBtn.forEach((plus) => {
-  plus.addEventListener("click", handlePlusBtnClick);
-});
-
-allRemoveI.forEach((remove) => {
-  remove.addEventListener("click", handleRemoveIClick);
-});
-
-/* ----------------------------------- */
-//!             FUNCTIONS              */
-/* ----------------------------------- */
-
-// decrease product quantity
-function handleMinusBtnClick(e) {
-  const countSpan = e.target.nextElementSibling;
-  if (parseInt(countSpan.textContent) > 1) {
-    countSpan.textContent--;
-    updateAmounts(e.target.closest(".item"));
-  }
-}
-
-// increase product quantity
-function handlePlusBtnClick(e) {
-  const countSpan = e.target.previousElementSibling;
-  if (parseInt(countSpan.textContent) < 10) {
-    countSpan.textContent++;
-    updateAmounts(e.target.closest(".item"));
-  } else {
-    alert("You can order a maximum of 10 of the same product in one purchase.");
-  }
-}
-
-function handleRemoveIClick(e) {
-  if (confirm("The product will be removed. Are you sure?")) {
-    const removedItem = e.target.closest("li");
-    const parentList = removedItem.parentElement;
-    parentList.removeChild(removedItem);
-    updateSummary(removedItem); // Update summary after removing the item
-  }
-}
-
-function updateSummary(removedItem) {
-  // If an item is removed, subtract its total from subtotal
-  if (removedItem) {
-    const removedItemTotal = parseFloat(
-      removedItem.querySelector("#item-total").textContent
+  function updateProductTotal(li) {
+    // Retrieve count and discount values from the list item
+    const count = parseInt(li.querySelector(".count").textContent);
+    const discount = parseFloat(
+      li.querySelector("#discount").textContent.replace(currency, "")
     );
-    const subTotal = parseFloat(subTotalSpan.textContent.replace(currency, ""));
-    const newSubTotal = subTotal - removedItemTotal;
-    subTotalSpan.textContent = newSubTotal.toFixed(2) + currency;
-
-    // Calculate VAT
-    const taxAmount = (newSubTotal * 0.19).toFixed(2);
-    taxSpan.textContent = taxAmount + currency;
-
-    // Update Shipping Fee
-    let shippingFee = 8;
-    if (newSubTotal > 400) {
-      shippingInfoSpan.textContent = "Shipping (Free)";
-      shippingFee = 0;
-    } else {
-      shippingInfoSpan.textContent = "Shipping";
-    }
-    shippingPriceSpan.textContent = shippingFee.toFixed(2) + currency;
-
-    // Calculate Total
-    const totalAmount = (
-      newSubTotal +
-      parseFloat(taxAmount) +
-      shippingFee
-    ).toFixed(2);
-    totalSpan.textContent = totalAmount + currency;
+    // Calculate item total and update the displayed value
+    const productTotal = (count * discount).toFixed(2);
+    li.querySelector("#item-total").textContent = productTotal + currency;
+    return parseFloat(productTotal);
   }
-}
 
-function updateAmounts() {
-  // Initialize subtotal
-  let subTotalSum = 0;
+  function updateSummary() {
+    // Initialize subtotal
+    let subtotal = 0;
+    // Get all list items containing items in the cart
+    const items = document.querySelectorAll(".items li");
+    // Iterate through each list item and update the subtotal
+    items.forEach(function (li) {
+      subtotal += updateProductTotal(li);
+    });
 
-  // Update each product total and calculate subtotal
-  allItemTotalSpan.forEach((itemTotalSpan, index) => {
-    if (itemTotalSpan) {
-      const discount = parseFloat(
-        document.querySelectorAll("#discount")[index].textContent
-      );
-      const count = parseFloat(allCountSpans[index].textContent);
-      const itemTotal = (discount * count).toFixed(2);
-      itemTotalSpan.textContent = itemTotal + currency;
-      subTotalSum += parseFloat(itemTotal);
+    // Calculate VAT, shipping, and total
+    const vat = (subtotal * 0.19).toFixed(2);
+
+    let shipping;
+    if (subtotal > 400) {
+      shipping = "0.00";
+      document.getElementById("shipping-info").textContent = "Shipping (FREE)";
+    } else if (subtotal === 0) {
+      shipping = "0.00";
+      const items = document.querySelector(".items");
+      items.textContent = "Your shopping cart is empty";
+      items.style.backgroundColor = "lightgreen";
+      items.style.margin = "0rem 0rem 2rem 2rem";
+      items.style.padding = "0rem";
+      items.style.textAlign = "center";
+    } else shipping = "8.00";
+
+    // add info into shipping if subtotal over 400€
+    if (shipping === "0.00") {
     }
+
+    const total = (subtotal + parseFloat(vat) + parseFloat(shipping)).toFixed(
+      2
+    );
+
+    // Update summary section with calculated values
+    document.getElementById("sub-total").textContent =
+      subtotal.toFixed(2) + currency;
+    document.getElementById("tax").textContent = vat + currency;
+    document.getElementById("shipping-price").textContent = shipping + currency;
+    document.getElementById("total").textContent = total + currency;
+  }
+
+  function removeItem(li) {
+    // Remove the specified list item and update the summary
+    li.remove();
+    updateSummary();
+  }
+
+  /* ---------------------------------- */
+  //!          Event Listeners          */
+  /* ---------------------------------- */
+  const plusButtons = document.querySelectorAll(".plus");
+  const minusButtons = document.querySelectorAll(".minus");
+  const removeButtons = document.querySelectorAll(".remove");
+  const allInputs = document.querySelectorAll("input");
+
+  // add or remove opacity => on and off focus inputs
+  allInputs.forEach((input) => {
+    input.addEventListener("focusin", (e) => {
+      e.target.parentElement.classList.remove("opacity");
+    });
+
+    input.addEventListener("focusout", (e) => {
+      if (!e.target.value || e.target.checked == false) {
+        e.target.parentElement.classList.add("opacity"); // only add opacity, if its empty
+      }
+    });
   });
 
-  // Update the subtotal
-  subTotalSpan.textContent = subTotalSum.toFixed(2) + currency;
+  // Increase the count when the plus button is clicked (+1)
+  plusButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const countSpan = button.parentElement.querySelector(".count");
+      let count = parseInt(countSpan.textContent);
+      count++;
+      countSpan.textContent = count;
+      // Update item total and summary
+      updateProductTotal(button.closest("li"));
+      updateSummary();
+    });
+  });
 
-  // Calculate the tax amount
-  const taxAmount = (subTotalSum * 0.19).toFixed(2);
-  taxSpan.textContent = taxAmount + currency;
+  // Decrease the count when the minus button is clicked (-1)
+  minusButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const countSpan = button.parentElement.querySelector(".count");
+      let count = parseInt(countSpan.textContent);
+      if (count > 1) {
+        count--;
+        countSpan.textContent = count;
+        // Update item total and summary
+        updateProductTotal(button.closest("li"));
+        updateSummary();
+      }
+    });
+  });
 
-  // Shipping Fee
-  let shippingFee = 8;
-  if (subTotalSum > 400) {
-    shippingInfoSpan.textContent = "Shipping (Free)";
-    shippingFee = 0;
-  } else {
-    shippingInfoSpan.textContent = "Shipping";
-  }
-  shippingPriceSpan.textContent = shippingFee.toFixed(2) + currency;
+  // Remove the item when the remove button is clicked
+  removeButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      removeItem(button.closest("li"));
+    });
+  });
 
-  // Calculate the Total (including VAT)
-  const totalAmount = (
-    subTotalSum +
-    parseFloat(taxAmount) +
-    shippingFee
-  ).toFixed(2);
-  totalSpan.textContent = totalAmount + currency;
-}
+  // Initially update the summary (default)
+  updateSummary();
+});
